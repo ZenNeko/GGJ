5e705aa5dffed4e2d171315d0b11b1e6ac4505f2
nicemare61 <icebreaker9961@gmail.com>
2024-01-27 11:28:35 +0700

    AddTag&BallManager

--- Assets/Script/Game Manager.cs
+++ Assets/Script/Game Manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gamemaneger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     private int PlayerHP = 3;
 
