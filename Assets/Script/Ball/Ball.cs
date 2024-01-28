2837a2d6b20ff9796298a13d3f8f636849bcf779
nicemare61 <icebreaker9961@gmail.com>
2024-01-28 11:26:24 +0700

    SpeedPower

--- Assets/Script/Ball/Ball.cs
+++ Assets/Script/Ball/Ball.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallBounce : MonoBehaviour
+public class Ball : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector3 lVelo;
